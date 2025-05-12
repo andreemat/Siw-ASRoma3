93c87339c9ad7d51120ee706503268ca5562d648
andreemat <andreamatteis03@gmail.com>
2025-05-12 18:25:28 +0200

    first commit

---
+++ src/main/java/siw/uniroma3/asroma3/SiwAsroma3Application.java
@@ -0,0 +1,13 @@
+package siw.uniroma3.asroma3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwAsroma3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwAsroma3Application.class, args);
+	}
+
+}
---
+++ src/test/java/siw/uniroma3/asroma3/SiwAsroma3ApplicationTests.java
@@ -0,0 +1,13 @@
+package siw.uniroma3.asroma3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiwAsroma3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
