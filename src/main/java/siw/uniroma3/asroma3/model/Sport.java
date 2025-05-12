f14f059dc114ca5c1c84d77e56e6b4f28291bc8d
andreemat <andreamatteis03@gmail.com>
2025-05-12 19:22:12 +0200

    aggiunta classe sport e squadra, aggiunto application.properties

--- src/main/java/siw/uniroma3/asroma3/model/Sport.java
+++ src/main/java/siw/uniroma3/asroma3/model/Sport.java
@@ -1,5 +1,75 @@
 package siw.uniroma3.asroma3.model;
 
+import java.util.List;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Sport {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String nome;
+	
+
+
+	@OneToMany(mappedBy="sport")
+	private List<Squadra> squadre;
+	
+	public Sport() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Squadra> getSquadre() {
+		return squadre;
+	}
+
+	public void setSquadre(List<Squadra> squadre) {
+		this.squadre = squadre;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Sport other = (Sport) obj;
+		return Objects.equals(nome, other.nome);
+	}
+	
+	
+	
+	
+	
+	
+	
 
 }
--- src/main/java/siw/uniroma3/asroma3/model/Squadra.java
+++ src/main/java/siw/uniroma3/asroma3/model/Squadra.java
@@ -1,5 +1,70 @@
 package siw.uniroma3.asroma3.model;
 
-public class Squadra {
+import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Squadra {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String nome;
+	private String categoria;
+	
+	@ManyToOne
+	private Sport sport;
+	
+	public Squadra() {
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public Sport getSport() {
+		return sport;
+	}
+	public void setSport(Sport sport) {
+		this.sport = sport;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(categoria, sport);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Squadra other = (Squadra) obj;
+		return Objects.equals(categoria, other.categoria) && Objects.equals(sport, other.sport);
+	}
+	
+	
+	
+	
 }
