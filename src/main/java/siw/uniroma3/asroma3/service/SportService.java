eda08ace444cbca3b55c5a85cc002a9793d417b9
andreemat <andreamatteis03@gmail.com>
2025-05-19 18:31:04 +0200

    aggiornamento repository e service

--- src/main/java/siw/uniroma3/asroma3/service/SportService.java
+++ src/main/java/siw/uniroma3/asroma3/service/SportService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import siw.uniroma3.asroma3.model.Prenotazione;
 import siw.uniroma3.asroma3.model.Sport;
 import siw.uniroma3.asroma3.repository.SportRepository;
 
@@ -13,11 +12,9 @@ import siw.uniroma3.asroma3.repository.SportRepository;
 public class SportService {
 	@Autowired SportRepository sportRepository;
 	
-	
 	public Sport getPrenotazineByid(Long id) {
 		return this.sportRepository.findById(id).orElse(null);
 	}
-	
 	public List<Sport> getAllPrenotazioni(){
 		return (List<Sport>)this.sportRepository.findAll();
 	}
