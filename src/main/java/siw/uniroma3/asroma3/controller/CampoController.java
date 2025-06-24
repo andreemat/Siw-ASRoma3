f973543762c819852cbcb5d5708018d16b0e7478
Biagou <biagou.02@gmail.com>
2025-06-24 18:11:33 +0200

    form associazione

--- src/main/java/siw/uniroma3/asroma3/controller/CampoController.java
+++ src/main/java/siw/uniroma3/asroma3/controller/CampoController.java
@@ -3,11 +3,14 @@ package siw.uniroma3.asroma3.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import jakarta.validation.Valid;
+import siw.uniroma3.asroma3.model.Campo;
 import siw.uniroma3.asroma3.model.Prenotazione;
 import siw.uniroma3.asroma3.service.AssociazioneService;
 import siw.uniroma3.asroma3.service.CampoService;
@@ -15,5 +18,28 @@ import siw.uniroma3.asroma3.service.SportService;
 
 @Controller
 public class CampoController {
+	@Autowired
+	SportService sportService;
+	@Autowired
+	CampoService campoService;
+	
+	
+	@GetMapping("/admin/registra-campo")
+	public String registraCampo(Model model) {
+		model.addAttribute("sports",sportService.getAllSport());
+		model.addAttribute("campo", new Campo());
+		return "admin/formCampo.html";
+	}
+	
+	@PostMapping("/admin/registra-campo")
+	public String salvaCampo(@Valid @ModelAttribute Campo campo, BindingResult bindingResult, Model model) {
+		if (bindingResult.hasErrors()) {
+			System.out.println("Errori di validazione sul form di registrazione associazione:");
+			bindingResult.getAllErrors().forEach(error -> System.out.println(error.getDefaultMessage()));
+			return "formCampo.html";
+		}
+		campoService.saveCampo(campo);
+		return "redirect:/";
+	}
 
 }
--- src/main/java/siw/uniroma3/asroma3/service/CampoService.java
+++ src/main/java/siw/uniroma3/asroma3/service/CampoService.java
@@ -26,5 +26,7 @@ public class CampoService {
 	public List<Campo> getAllByAssociazioneESport(Long idA,Long idS) {
 		return this.campoRepository.findBySportIdAndAssociazioneId(idS, idA);
 	}
-	
+	public void saveCampo(Campo campo) {
+		this.campoRepository.save(campo);
+	}
 }
